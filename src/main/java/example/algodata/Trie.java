4b502526750e3524a6368cf86cebcd8ad0ac5acd
seongchan cho <wordnumperiod@gmail.com>
2023-10-10 10:12:12 +0900

    add: Trie 구현 코드 참고 링크 추가

--- src/main/java/example/algodata/Trie.java
+++ src/main/java/example/algodata/Trie.java
@@ -1,7 +1,6 @@
 package example.algodata;
 
-import org.w3c.dom.Node;
-
+//https://velog.io/@cgw0519/%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-%ED%8A%B8%EB%9D%BC%EC%9D%B4
 import java.util.HashMap;
 import java.util.Map;
 
